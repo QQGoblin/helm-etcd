f4ff3d7b8a1e0d68ca26d4af503a1e8b7450b284
LinQing2017 <m15651620957@163.com>
2022-05-08 19:44:06 +0800

    fix：readme.md

--- hack/gobuild.sh
+++ hack/gobuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export HTTPS_PROXY=http://172.28.235.169:10889
-export HTTP_PROXY=http://172.28.235.169:10889
 
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0  GO111MODULE=on GOPROXY=https://goproxy.cn go mod tidy
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0  GO111MODULE=on GOPROXY=https://goproxy.cn go mod vendor
